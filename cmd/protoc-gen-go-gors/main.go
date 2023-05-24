c819385db4f8e989dd95ac946c422162e6126762
xiaoyan648 <1425895909@qq.com>
2023-05-24 20:39:42 +0800

    fix: 自定义参数无法解析bug

--- cmd/protoc-gen-go-gors/main.go
+++ cmd/protoc-gen-go-gors/main.go
@@ -9,11 +9,11 @@ import (
 )
 
 var (
-	showVersion       = flag.Bool("version", false, "print the version and exit")
-	omitempty         = flag.Bool("omitempty", true, "omit if google.api is empty")
-	bizcode           = flag.Bool("bizcode", true, "open biz code model")
-	setGinErr         = flag.Bool("setGinErr", true, "if has error, set to gin context")
-	setPayloadWhenErr = flag.Bool("setPayloadWhenErr", true, "if has error, set req payload to gin context, can use for log")
+	showVersion = flag.Bool("version", false, "print the version and exit")
+	omitempty   = flag.Bool("omitempty", true, "omit if google.api is empty")
+	bizcode     = flag.Bool("bizcode", true, "open biz code model")
+	setginerr   = flag.Bool("setginerr", true, "if has error, set to gin context")
+	setpayload  = flag.Bool("setpayload", true, "if has error, set req payload to gin context, can use for log")
 )
 
 type Config struct {
@@ -29,17 +29,18 @@ func main() {
 		fmt.Printf("protoc-gen-go-http %v\n", release)
 		return
 	}
-	// init config
-	config := Config{
-		omitempty:         *omitempty,
-		bizCode:           *bizcode,
-		setGinErr:         *setGinErr,
-		setPayloadWhenErr: *setPayloadWhenErr,
-	}
+
 	// protoc run plugin
 	protogen.Options{
 		ParamFunc: flag.CommandLine.Set,
 	}.Run(func(gen *protogen.Plugin) error {
+		// init config
+		config := Config{
+			omitempty:         *omitempty,
+			bizCode:           *bizcode,
+			setGinErr:         *setginerr,
+			setPayloadWhenErr: *setpayload,
+		}
 		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 		for _, f := range gen.Files {
 			if !f.Generate {
