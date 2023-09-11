096cf461f133ada38eb45800ec1189d2c269e22f
xiaoyan648 <1425895909@qq.com>
2023-09-11 14:25:13 +0800

    feat(tag): remove lowcase logic

--- cmd/protoc-go-inject-tag/parse.go
+++ cmd/protoc-go-inject-tag/parse.go
@@ -15,11 +15,11 @@ func tagFromComment(comment string) (tag string) {
 }
 
 func defaultTag(fieldName string, jsonOmit bool) string {
-	camelName := convertToSnakeCase(fieldName)
+	// camelName := convertToSnakeCase(fieldName)
 	if jsonOmit {
-		return fmt.Sprintf(`form:"%[1]s" json:"%[1]s,omitempty"`, camelName)
+		return fmt.Sprintf(`form:"%[1]s" json:"%[1]s,omitempty"`, fieldName)
 	}
-	return fmt.Sprintf(`form:"%[1]s" json:"%[1]s"`, camelName)
+	return fmt.Sprintf(`form:"%[1]s" json:"%[1]s"`, fieldName)
 }
 
 type tagItem struct {
